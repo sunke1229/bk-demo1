dad7878829742920e68c72994ebed7188ba99c22
sunke <sunke@beyondcent.com>
2019-03-15 18:42:14 +0800

    日志级别

--- webproject/src/main/java/com/bocloud/blueking/web/rest/inspection/InspectController.java
+++ webproject/src/main/java/com/bocloud/blueking/web/rest/inspection/InspectController.java
@@ -27,7 +27,6 @@ import com.tencent.bk.utils.json.JsonUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
